12ded34d0fee0528db8ae2ca26aae0a658e535ce
westzero77 <westzero713@gmail.com>
2024-03-24 20:27:05 +0900

    quiz/05-1/ 문제풀이 앞에서 배운 뷰 템플릿 속성 중 {{ }} 문법과 v-bind 디렉티브를 이용한 데이터 바인딩, v-on 디렉티브와 v-if 디렉티브를 직접 사용해 봅시다.

--- quiz/05-1/sample/js/app.js
+++ quiz/05-1/sample/js/app.js
@@ -4,12 +4,13 @@ var app = new Vue({
     message : 'Hello Vue.js',
     // 할 일 #1
     // 새로운 데이터 속성을 1개 추가하고, 머스타쉬 {{ }} 데이터 바인딩을 이용하여 화면에 표시해보세요.
+    newData : 'new Data',
 
-    uid: '10',
+    uid: '20',
     // 할 일 #2
     // uid를 변경하고 해당 uid의 변경 여부를 크롬 개발자 도구의 '화면 요소 검사 기능'으로 확인해보세요. p 태그의 id 값을 검사.
 
-    flag: true
+    flag: false
     // 할 일 #4
     // 위 flag 값을 false로 변경했을 때 화면에 어떤 영향을 주는지 확인해보세요.
   },
@@ -25,5 +26,8 @@ var app = new Vue({
 
     // 할 일 #3
     // eventMethod를 하나 추가하고 index.html의 #3 영역에 해당 이벤트를 실행하는 버튼을 추가하세요.
+    clickNewBtn: function () {
+      console.log("clickNewBtn 버튼 클릭");
+    }
   }
 });
